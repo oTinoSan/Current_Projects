d49fd0a4abcc5cb1a2106c9bfc54b5163133e603
otinosan <santinosini@gmail.com>
2024-01-25 21:37:46 -0500

    update programs

--- pymavlink/etc_commands.py
+++ pymavlink/etc_commands.py
@@ -1,5 +1,94 @@
-# Disarm the vehicle in flight
+################################
+# Disarm the vehicle in flight #
+################################
 the_connection.mav.command_long_send(
     the_connection.target_system, the_connection.target_component, 
     mavutil.mavlink.MAV_CMD_COMPONENT_ARM_DISARM, 0, 0, 21196, 0, 0, 0, 0, 0)
 
+################################
+# Movement command to lat/long #
+################################
+# https://mavlink.io/en/messages/common.html#GLOBAL_POSITION_INT
+# https://ardupilot.org/dev/docs/copter-commands-in-guided-mode.html#movement-command-details
+
+def move_drone(drone_connection):
+    drone_connection.mav.send(
+        mavutil.mavlink.MAVLink_set_position_target_global_int_message(
+        10,
+        drone_connection.target_system, 
+        drone_connection.target_component, 
+        mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT_INT, 
+        int(0b110111111000), 
+        -353621474, 
+        1491651746, 
+        600, 
+        0, 
+        0, 
+        0, 
+        0, 
+        0, 
+        0, 
+        0, 
+        0))
+
+        # Enable message tracking
+    while True:
+        msg = drone_connection.recv_match(
+            # type = 'NAV_CONTROLLER_OUTPUT', blocking=True)    # returns waypoint info
+            type = 'GLOBAL_POSITION_INT', blocking=True)    # returns GPS position
+        print(msg)
+
+################################
+# Movement command to waypoint #
+################################
+# https://ardupilot.org/dev/docs/copter-commands-in-guided-mode.html#copter-commands-in-guided-mode-set-position-target-local-ned
+# https://mavlink.io/en/messages/common.html#LOCAL_POSITION_NED
+# Timesinceboot is in milliseconds
+
+def move_drone(drone_connection):
+    drone_connection.mav.send(
+        mavutil.mavlink.MAVLink_set_position_target_local_ned_message(
+        10, 
+        drone_connection.target_system, 
+        drone_connection.target_component, 
+        mavutil.mavlink.MAV_FRAME_LOCAL_NED, 
+        int(0b110111111000), 
+        20, 
+        0, 
+        -10, 
+        0, 
+        0, 
+        0, 
+        0, 
+        0, 
+        0, 
+        0, 
+        0))
+
+    # Enable message tracking
+    while True:
+        msg = drone_connection.recv_match(
+            # type = 'NAV_CONTROLLER_OUTPUT', blocking=True)    # returns waypoint info
+            type = 'LOCAL_POSITION_NED', blocking=True)    # returns GPS position
+        print(msg)
+
+
+###############################
+# Rotate 360 degrees movement #
+###############################
+# https://ardupilot.org/copter/docs/common-mavlink-mission-command-messages-mav_cmd.html#mav-cmd-condition-yaw
+
+def rotate_360(drone_connection):
+    drone_connection.mav.command_long_send(
+            drone_connection.target_system, 
+            drone_connection.target_component,
+            mavutil.mavlink.MAV_CMD_CONDITION_YAW, 
+            0, 
+            360,    # degrees 0 - 360 
+            10,    # speed degrees per second
+            1,    # direction: -1: counter clockwise, 0: shortest direction, 1: clockwise
+            1,    # 0: absolute angle, 1: relative offset
+            0,0,0)
+
+    msg = drone_connection.recv_match(type='COMMAND_ACK', blocking=True)
+    print(msg)
--- pymavlink/movement.py
+++ pymavlink/movement.py
@@ -1,22 +1,102 @@
 from pymavlink import mavutil
+import time
+import math
+
+# Assumes that the drone is already in the air
 
 # Start a connection listening on a UDP port
-the_connection = mavutil.mavlink_connection('udpin:localhost:14551')
+drone_connection = mavutil.mavlink_connection('udpin:localhost:14551')
 
-# Wait for the first heartbeat 
-# This sets the system and component ID of remote system for the link
-the_connection.wait_heartbeat()
-print("Heartbeat from system (system %u component %u)" % (the_connection.target_system, the_connection.target_component))
+def connect_to_drone():
+    # Start a connection listening on a UDP port
+    drone_connection = mavutil.mavlink_connection('udpin:localhost:14551')
 
-# timesinceboot is in milliseconds
-the_connection.mav.send(mavutil.mavlink.MAVLink_set_position_target_local_ned_message(10, the_connection.target_system, 
-                        the_connection.target_component, mavutil.mavlink.MAV_FRAME_LOCAL_NED, int(0b100111111111), 20, 0, -10, 0, 0, 0, 0, 0, 1.0, 1.57, 0))
+    # This sets the system and component ID of remote system for the link
+    drone_connection.wait_heartbeat()
+    print("Heartbeat from system (system %u component %u)" % 
+         (drone_connection.target_system, drone_connection.target_component))
+    
+    return drone_connection
 
-while True:
-    # # to return waypoint info, while statement can be used to check if a certain waypoint is met
-    # msg = the_connection.recv_match(
-    #     type = 'NAV_CONTROLLER_OUTPUT', blocking=True)
-    # to return GPS info, while statement can be used to check if the GPS position is reached
-    msg = the_connection.recv_match(
-        type = 'LOCAL_POSITION_NED', blocking=True)
+def switch_to_guided_mode(drone_connection):
+    # Switch to GUIDED mode
+    # 1 is MAV_MODE_FLAG_CUSTOM_MODE_ENABLED (for copter), 4 is flight mode guided
+    drone_connection.mav.command_long_send(
+        drone_connection.target_system, drone_connection.target_component,
+        mavutil.mavlink.MAV_CMD_DO_SET_MODE, 0, 1, 4, 0, 0, 0, 0, 0)
+
+    msg = drone_connection.recv_match(type='COMMAND_ACK', blocking=True)
     print(msg)
+
+def arm_vehicle(drone_connection):
+    # Arm the vehicle
+    drone_connection.mav.command_long_send(
+        drone_connection.target_system, drone_connection.target_component, 
+        mavutil.mavlink.MAV_CMD_COMPONENT_ARM_DISARM, 0, 1, 0, 0, 0, 0, 0, 0)    # 1 is MAV_ARMED
+
+    msg = drone_connection.recv_match(type='COMMAND_ACK', blocking=True)
+    print(msg)
+
+def takeoff(drone_connection):
+    # Takeoff to 10m
+    drone_connection.mav.command_long_send(
+        drone_connection.target_system, drone_connection.target_component,
+        mavutil.mavlink.MAV_CMD_NAV_TAKEOFF, 0, 0, 0, 0, 0, 0, 0, 10) #altitude is in meters
+
+    msg = drone_connection.recv_match(type='COMMAND_ACK', blocking=True)
+    print(msg)
+
+def move_drone(drone_connection):
+    drone_connection.mav.send(
+        mavutil.mavlink.MAVLink_set_position_target_local_ned_message(
+        10, 
+        drone_connection.target_system, 
+        drone_connection.target_component, 
+        mavutil.mavlink.MAV_FRAME_LOCAL_NED, 
+        int(0b110111111000), 
+        20, 
+        0, 
+        -10, 
+        0, 
+        0, 
+        0, 
+        0, 
+        0, 
+        0, 
+        0, 
+        0))
+
+    # Enable message tracking
+    while True:
+        msg = drone_connection.recv_match(
+            # type = 'NAV_CONTROLLER_OUTPUT', blocking=True)    # returns waypoint info
+            type = 'LOCAL_POSITION_NED', blocking=True)    # returns GPS position
+        print(msg)
+
+def rotate_360(drone_connection):
+    drone_connection.mav.command_long_send(
+            drone_connection.target_system, 
+            drone_connection.target_component,
+            mavutil.mavlink.MAV_CMD_CONDITION_YAW, 
+            0, 
+            360,    # degrees 0 - 360 
+            10,    # speed degrees per second
+            1,    # direction: -1: counter clockwise, 0: shortest direction, 1: clockwise
+            1,    # 0: absolute angle, 1: relative offset
+            0,0,0)
+
+    msg = drone_connection.recv_match(type='COMMAND_ACK', blocking=True)
+    print(msg)
+
+def main():
+    drone = connect_to_drone()
+    switch_to_guided_mode(drone)
+    arm_vehicle(drone) 
+    takeoff(drone)
+    move_drone(drone)
+    rotate_360(drone)
+
+# Usage
+if __name__ == "__main__":
+    main()
+
