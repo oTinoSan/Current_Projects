55b087302580b51f3c120809edea5060b9a6bd08
otinosan <santinosini@gmail.com>
2024-02-04 19:32:47 -0500

    edit layout

--- classes/struct_algs/colatz_progression.py
+++ classes/struct_algs/colatz_progression.py
@@ -1,11 +1,6 @@
-
 """
 Extend the Progression class we have discussed at the end of Chapter to create a Colatz progression. The progression starts with a starting value, which is passed into the constructor of the progression. The next number depends on the previous value: if the previous value is an even number, the next number is half of it; if the previous number is an odd number, the next number is three times this number plus one. The progression ends when it reach the value of 1. For example, the Colatz progression starting with 7 is: 7, 22, 11, 34, 17, 52, 26, 13, 40, 20, 10, 5, 16, 8, 4, 2, 1.
 """
-
-
-
-
 class ColatzProgression (Progression):
     """Iterator producing a generalized Fibonacci progression."""
 
